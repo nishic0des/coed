ae15b8228495c9608b26e30ca462b069497e6292
Nishi <nishi241105@gmail.com>
2025-12-13 15:01:04 +0530

    dashboard-ui

---
+++ modules/dashboard/types.ts
@@ -0,0 +1,21 @@
+export interface User {
+	id: string;
+	name: string;
+	email: string;
+	image: string;
+	role: string;
+	createAt: Date;
+	updatedAt: Date;
+}
+
+export interface Project {
+	id: string;
+	title: string;
+	description: string;
+	template: string;
+	createdAt: Date;
+	updatedAt: Date;
+	userId: string;
+	user: User;
+	StarMark: { isMarked: boolean }[];
+}
